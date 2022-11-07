624c9b53c26eeac2fdea33e803543d5dd172a506
NurtileuAblazimov <nurtileu@tredo.co>
2022-11-07 21:13:25 +0600

    TRACKER-1: init google map

--- app/src/main/java/kz/ablazim/runtracker/auth/presentation/compose/HomeScreen.kt
+++ app/src/main/java/kz/ablazim/runtracker/auth/presentation/compose/HomeScreen.kt
@@ -1,21 +1,40 @@
 package kz.ablazim.runtracker.auth.presentation.compose
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.ktx.awaitMap
+import kotlinx.coroutines.launch
+import kz.ablazim.runtracker.utils.rememberMapViewWithLifecycle
 
 @Composable
 fun HomeScreen() {
+    val mapView = rememberMapViewWithLifecycle()
+    val coroutineScope = rememberCoroutineScope()
+
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+            .padding(bottom = 36.dp)
     ) {
-        Text(text = "This is Home Screen", textAlign = TextAlign.Center)
+        AndroidView(factory = { mapView }) { mapView ->
+            coroutineScope.launch {
+                val map = mapView.awaitMap()
+                map.uiSettings.isZoomControlsEnabled = true
+                val location = LatLng(60.026, 42.023)
+
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+            }
+        }
     }
 }
